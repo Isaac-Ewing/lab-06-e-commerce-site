03fb2a229ed9dd4d3f5e4754207c83c71541f1de
Isaac <isaac.ewing3@gmail.com>
2021-05-10 16:14:10 -0600

    still trying to get images to show

--- data/games.js
+++ data/games.js
@@ -1,7 +1,7 @@
 const Spelunky2 = {
     id: 'Spelunky2',
     name: 'Spelunky 2',
-    image: '../assets/spelunky-2.jpg',
+    image: 'https://cdn.cloudflare.steamstatic.com/steam/apps/418530/capsule_616x353.jpg?t=1608620216',
     description: 'A game that makes you hate yourself',
     category: 'Rouge-like',
     price: '30'
@@ -10,7 +10,7 @@ const Spelunky2 = {
 const Valheim = {
     id: 'valheim',
     name: 'Valheim',
-    image: '../assets/valheim.jpg',
+    image: 'https://images.squarespace-cdn.com/content/v1/5e203941ee6ea226e307532c/1612260136935-HEXYGD6GIJ1SNZ5WCZGI/ke17ZwdGBToddI8pDm48kAIua_kSKNK6jBaG8AUq4Ih7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0uZ4nTVmo-7CsBThMahHfB0BoGS5mHGJvCx-swYOZ3mVCkNZWN7tJPeKIX099Fba3A/Valheim+Keyart+Squad+enjoying+the+vista.png?format=2500w',
     description: 'Like Minecraft, but less to do and you are vikings',
     category: 'Survival',
     price: '20'
@@ -19,7 +19,7 @@ const Valheim = {
 const AmongUs = {
     id: 'amongus',
     name: 'Among Us',
-    image: '../assets/amongus.jpg',
+    image: 'https://cdn.akamai.steamstatic.com/steam/apps/945360/capsule_616x353.jpg?t=1611793703',
     description: 'Lie to everyone',
     category: 'Deception',
     price: '5'
@@ -28,7 +28,7 @@ const AmongUs = {
 const Stellaris = {
     id: 'stellaris',
     name: 'Stellaris',
-    image: '../assets/stellaris.jpg',
+    image: 'https://fanatical.imgix.net/product/original/eef0ed9c-ff32-48d7-8416-2c087feb54fb.jpg?auto=compress,format&w=400&fit=max',
     description: 'Take over the galaxy, fight other alien races',
     category: 'Grand-Strategy',
     price: '40'
@@ -37,7 +37,7 @@ const Stellaris = {
 const Phasmophobia = {
     id: 'phasmophobia',
     name: 'Phasmophobia',
-    image: '../assets/phasmophobia.jpg',
+    image: 'https://cdn.cloudflare.steamstatic.com/steam/apps/739630/capsule_616x353.jpg?t=1613740031',
     description: 'Hunt ghosts with friends',
     category: 'Horror',
     price: '30'
