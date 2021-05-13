9619666dc7bd519b73a5a53df48a9b714472e6d1
Isaac <isaac.ewing3@gmail.com>
2021-05-13 09:26:08 -0600

    linked shooping cart to local storage

--- cart/cart.js
+++ cart/cart.js
@@ -1,12 +1,14 @@
 import { renderCartItem } from './render-cart-items.js';
-import cartItems from '../cart-items.js';
+import { getCart } from '../local-storage-utils.js';
 import { calcItemTotal, findById } from '../utils.js';
 import games from '../data/games.js';
 
 const anchor = document.querySelector('tbody');
 const total = document.getElementById('total');
+const orderButton = document.getElementById('button');
 
 let totalPrice = 0;
+let cartItems = getCart();
 
 for (let item of cartItems) {
     const tableRow = renderCartItem(item, games);
@@ -15,4 +17,11 @@ for (let item of cartItems) {
     anchor.append(tableRow);
 }
 
-total.textContent = `${totalPrice}`;
+orderButton.style.opacity = 0;
+total.textContent = '$' + `${totalPrice}`;
+if (totalPrice > 0) {
+    orderButton.style.opacity = 1;
+    orderButton.addEventListener('click', () => {
+        localStorage.removeItem('CART');
+    });
+}
